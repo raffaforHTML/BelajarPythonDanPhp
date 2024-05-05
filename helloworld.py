057e89b162954bd6fdc699e2bb3e874466adc9a5
R A F F A   A R R A Y A <155284553+raffaforHTML@users.noreply.github.com>
2024-05-05 13:32:39 +0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python
 
-print("Hell, World")
+print("MY INFO")
+print("1. NAMA: RAFFA ARRAYA")
+print("2. USIA: 13")
+print("3. ALAMAT: PADANG, PARIAMAN")
+print("4. EMAIL: raffaarrayainfo@yaho.com")
